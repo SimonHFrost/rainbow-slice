a05d1fe10098e49a70d7b9ff978428e4cec1bd00
SimonHFrost <simonhfrost@gmail.com>
2014-03-14 14:28:30 +1300

    create walls for arena

--- sceneInitializer.js
+++ sceneInitializer.js
@@ -1,6 +1,7 @@
 var sceneInitializer = {
   FLOOR_DIMENSIONS : 3000,
   ENEMY_BORDER_WIDTH : 1000,
+  BASIC_MATERIAL : new THREE.MeshLambertMaterial( { shading: THREE.FlatShading, color: 0xFF00FF }),
 
   initCameraAndLights : function() {
     camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500000 );
@@ -16,31 +17,67 @@ var sceneInitializer = {
   },
 
   initSceneObjects : function() {
-    var floorWidth = this.FLOOR_DIMENSIONS * 2;
-    var floorDepth = this.FLOOR_DIMENSIONS * 2;
-    var material = new THREE.MeshLambertMaterial( { shading: THREE.FlatShading, color: 0x00CC33 } );
-    floor = new THREE.Mesh( new THREE.PlaneGeometry( floorWidth, floorDepth ), material );
-    floor.position.y = - 100;
-    floor.rotation.x = - Math.PI / 2;
-    scene.add( floor );
-
-    var cubeSize = 400;
-    mainCube = new THREE.Mesh( new THREE.CubeGeometry( cubeSize, cubeSize, cubeSize ), new THREE.MeshLambertMaterial( { shading: THREE.FlatShading, color: 0x0000FF } ) );
-    scene.add( mainCube );
-
+    this.makeArena();
+    this.makeMainCube();
     this.makeEnemies();
     this.makeSkyBox();
   },
 
+  makeArena : function() {
+    var floorWidth = this.FLOOR_DIMENSIONS * 2;
+    var floorDepth = this.FLOOR_DIMENSIONS * 2;
+
+    var material = new THREE.MeshLambertMaterial( { shading: THREE.FlatShading, color: 0x00CC33 } );
+    floor = new THREE.Mesh( new THREE.PlaneGeometry( floorWidth, floorDepth), material );
+    floor.position.y = - 100;
+    floor.rotation.x = - Math.PI / 2;
+    scene.add( floor );
+
+    this.makeWalls();
+  },
+
+  makeWalls : function() {
+    var wallWidth = 400;
+    var wallDepth = 200;
+    var floorBoundry = this.FLOOR_DIMENSIONS + wallWidth/2;
+    var wallTemplate = new THREE.Mesh(new THREE.CubeGeometry(wallWidth, wallDepth, this.FLOOR_DIMENSIONS*2), this.BASIC_MATERIAL);
+
+    var eastWall = wallTemplate.clone();
+    eastWall.position.x = floorBoundry;
+    scene.add(eastWall);
+
+    var westWall = wallTemplate.clone();
+    westWall.position.x = -floorBoundry;
+    scene.add(westWall);
+
+    var southWall = wallTemplate.clone();
+    southWall.position.z = -floorBoundry;
+    southWall.position.x = 0;
+    southWall.rotation.y = Math.PI / 2;
+    scene.add(southWall);
+
+    var northWall = wallTemplate.clone();
+    northWall.position.z = floorBoundry;
+    northWall.position.x = 0;
+    northWall.rotation.y = Math.PI / 2;
+    scene.add(northWall);
+  },
+
+  makeMainCube : function() {
+    var cubeSize = 400;
+    mainCube = new THREE.Mesh( new THREE.CubeGeometry( cubeSize, cubeSize, cubeSize ), this.BASIC_MATERIAL);
+    scene.add(mainCube);
+  },
+
   makeEnemies : function() {
     var numEnemiesPerSide = 5;
-    var numEnemies = 10;
-
-    var x, z;
+    var numEnemies = 11;
 
     while (numEnemies--) {
         var somethingAlreadyAtLocation = true;
 
+        var x;
+        var z;
         while(somethingAlreadyAtLocation) {
           x = Math.floor(Math.random() * numEnemiesPerSide);
           z = Math.floor(Math.random() * numEnemiesPerSide);
@@ -48,10 +85,6 @@ var sceneInitializer = {
           somethingAlreadyAtLocation = _.some(main.enemyMeshList, function(enemy) {
             return enemy.gridX == x && enemy.gridZ == z;
           });
-
-          if (x === 3 && z === 3) {
-            somethingAlreadyAtLocation = true;
-          }
         }
 
         this.instantiateEnemy(x, z);
