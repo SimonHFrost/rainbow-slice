879e3064555b21a90cd873bad7016b89c0e967cc
SimonHFrost <simonhfrost@gmail.com>
2014-03-14 17:55:11 +1300

    centre align title and health left

--- materials.js
+++ materials.js
@@ -2,7 +2,7 @@ var materials = {
   BASIC: new THREE.MeshLambertMaterial( { shading: THREE.FlatShading, color: 0xFF00FF } ),
   ENEMY: new THREE.MeshLambertMaterial( { shading: THREE.FlatShading, color: 0xFFFFFF } ),
   DEAD: new THREE.MeshLambertMaterial( { shading: THREE.FlatShading, color: 0xFF0000 } ),
-  SKY: new THREE.MeshBasicMaterial( {color: 0xCCCCFF, side: THREE.BackSide} ),
+  SKY: new THREE.MeshBasicMaterial( {color: 'black', side: THREE.BackSide} ),
   GROUND: new THREE.MeshBasicMaterial( {shading: THREE.FlatShading, color: 0x009933 }),
   BULLET: new THREE.MeshLambertMaterial( { shading: THREE.FlatShading, color: 0xFFFFFF } ),
 };
