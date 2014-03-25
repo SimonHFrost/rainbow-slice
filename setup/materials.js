3022fa7a2a9055673f3b3b3b3b9400353e95bf9c
SimonHFrost <simonhfrost@gmail.com>
2014-03-25 14:21:19 +1300

    change color of death texture

--- setup/materials.js
+++ setup/materials.js
@@ -1,8 +1,8 @@
 var materials = {
-  BASIC: new THREE.MeshLambertMaterial( { shading: THREE.FlatShading, color: 0xFF00FF } ),
+  BASIC: new THREE.MeshLambertMaterial( { shading: THREE.SmoothShading, color: 0x000000 } ),
   ENEMY: new THREE.MeshLambertMaterial( { shading: THREE.FlatShading, color: 0xFFFFFF } ),
   DEAD: new THREE.MeshLambertMaterial( { shading: THREE.FlatShading, color: 0xFF0000 } ),
   SKY: new THREE.MeshBasicMaterial( {color: 'black', side: THREE.BackSide} ),
   GROUND: new THREE.MeshBasicMaterial( {shading: THREE.SmoothShading, color: 0x009933 }),
-  BULLET: new THREE.MeshLambertMaterial( { shading: THREE.FlatShading, color: 0xFFFFFF } ),
+  BULLET: new THREE.MeshLambertMaterial( { shading: THREE.FlatShading, color: 0xFFFFFF } )
 };
