e80168fe58991916d5a071e3ef92cc5f69f1bc31
SimonHFrost <simonhfrost@gmail.com>
2014-04-04 18:13:13 +1300

    classify materials

--- public/setup/materials.js
+++ public/setup/materials.js
@@ -1,8 +1,8 @@
-var materials = {
-  BASIC: new THREE.MeshLambertMaterial( { shading: THREE.SmoothShading, color: 0x000000 } ),
-  ENEMY: new THREE.MeshLambertMaterial( { shading: THREE.FlatShading, color: 0xFFFFFF } ),
-  DEAD: new THREE.MeshBasicMaterial( { shading: THREE.SmoothShading, color: 0x000000 } ),
-  SKY: new THREE.MeshBasicMaterial( {color: 'black', side: THREE.BackSide} ),
-  GROUND: new THREE.MeshBasicMaterial( {shading: THREE.SmoothShading, color: 0x009933 }),
-  BULLET: new THREE.MeshLambertMaterial( { shading: THREE.FlatShading, color: 0xFFFFFF } )
-};
+function Materials() {
+  this.BASIC = new THREE.MeshLambertMaterial( { shading: THREE.SmoothShading, color: 0x000000 } );
+  this.ENEMY = new THREE.MeshLambertMaterial( { shading: THREE.FlatShading, color: 0xFFFFFF } );
+  this.DEAD = new THREE.MeshBasicMaterial( { shading: THREE.SmoothShading, color: 0x000000 } );
+  this.SKY = new THREE.MeshBasicMaterial( {color: 'black', side: THREE.BackSide} );
+  this.GROUND = new THREE.MeshBasicMaterial( {shading: THREE.SmoothShading, color: 0x009933 });
+  this.BULLET = new THREE.MeshLambertMaterial( { shading: THREE.FlatShading, color: 0xFFFFFF } );
+}
