5d6617d2c3621ef994a6056d115da4ad72b0b5b7
SimonHFrost <simonhfrost@gmail.com>
2014-04-08 16:29:29 +1200

    change io connection info for troubleshooting production

--- public/networking/connectionCount.js
+++ public/networking/connectionCount.js
@@ -1,5 +1,5 @@
 function ConnectionCount(connectionCount) {
-  var socket = io.connect('http://localhost');
+  var socket = io.connect('http://rainbowslice.com');
   socket.on('connectionCount', function (data) {
     console.log(data.connectionCount + ' players have connected.');
     connectionCount.html(data.connectionCount + ' connections');
