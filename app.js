03f68ace4105cdf91143da5e6fbc52a4610f172e
SimonHFrost <simonhfrost@gmail.com>
2014-04-10 11:39:08 +1200

    provide DEBUG_MODE config for node params

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ var http = require('http');
 var static = require('node-static');
 var socketIO = require('socket.io');
 
-var folder = new(static.Server)('./public');
+var config = require('./config');
+
+var port = config.DEBUG_MODE ? 8080 : 32491;
+var publicFolder = config.DEBUG_MODE ? './public' : '../public';
+console.log('Debug mode = ' + config.DEBUG_MODE);
+
+var folder = new(static.Server)(publicFolder);
 httpServer = http.createServer(function (request, response) {
   folder.serve(request, response);
-}).listen(8080);
+}).listen(port);
 
 io = socketIO.listen(httpServer);
 
@@ -14,6 +20,5 @@ io.sockets.on('connection', function (socket) {
   console.log('Client Connected!');
   connectionCount++;
 
-  // socket.emit('connectionCount', { connectionCount: connectionCount });
   io.sockets.emit('connectionCount', { connectionCount: connectionCount });
 });
---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {};
+
+config.DEBUG_MODE = true;
+
+module.exports = config;
