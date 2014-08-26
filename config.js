77de000f599b6594167b6b7352379a472f2dac4c
SimonHFrost <simonhfrost@gmail.com>
2014-08-26 17:22:32 +0200

    debug deployment

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 var config = {};
 
-config.DEBUG_MODE = process.env.RAINBOWSLICE_DEBUGMODE;
+// config.DEBUG_MODE = process.env.RAINBOWSLICE_DEBUGMODE;
+
+config.DEBUG_MODE = false;
 console.log('Debug mode = ' + config.DEBUG_MODE);
 
 module.exports = config;
