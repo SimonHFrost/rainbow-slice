29995ab4fa05af817cb0661f3d96c3398c26053f
SimonHFrost <simonhfrost@gmail.com>
2014-08-27 09:42:33 +0200

    revert env variable attempts

--- config.js
+++ config.js
@@ -1,9 +1,5 @@
 var config = {};
 
-if (process.env.RAINBOWSLICE_DEBUGMODE) {
-  config.DEBUG_MODE = true;
-} else {
-  config.DEBUG_MODE = false;
-}
+config.DEBUG_MODE = false;
 
 module.exports = config;
