5bdee00a66b5732e968bbce3b971a16eff01eaa8
SimonHFrost <simonhfrost@gmail.com>
2014-08-28 21:18:23 +0200

    move remaining functionality from SceneInitializer to SceneObjects

--- public/sceneObjects/sceneObjects.js
+++ public/sceneObjects/sceneObjects.js
@@ -5,6 +5,13 @@ window.SceneObjects = (function() {
 
   function SceneObjects(scene) {
     this.scene = scene;
+
+    this.FLOOR = -100;
+    this.ISLAND_FLOOR = this.FLOOR - 1000;
+
+    this.initCameraAndLights();
+    this.initSceneObjects();
+
     this.network = new Network();
     this.story = new Story(scene, this.network);
     SceneObjects.updatableObjects.push(this.story);
@@ -26,6 +33,46 @@ window.SceneObjects = (function() {
   SceneObjects.morphs = [];
   SceneObjects.enemySpawner = '';
   SceneObjects.updatableObjects = [];
+
+  SceneObjects.prototype.initCameraAndLights = function() {
+    var camera = new THREE.PerspectiveCamera(45, Main.VIEWER_WIDTH / Main.VIEWER_HEIGHT, 1, 200000);
+    camera.position.y = 5000;
+    camera.position.z = 8000;
+    camera.lookAt(new THREE.Vector3(0,0,0));
+    SceneObjects.camera = camera;
+
+    var pointLight = new THREE.SpotLight(0xFFFFFF);
+    pointLight.position.set(5000, 5000, 5000);
+    pointLight.castShadow = true;
+
+    pointLight.shadowCameraNear = 50;
+    pointLight.shadowCameraFar = 250000;
+    this.scene.add(pointLight);
+
+    var ambientLight = new THREE.AmbientLight(0x808080);
+    this.scene.add(ambientLight);
+  };
+
+  SceneObjects.prototype.initSceneObjects = function() {
+    new IslandInitializer(this.scene).makeIsland(this.FLOOR, this.ISLAND_FLOOR);
+    this.makePlayer();
+    this.makeSkyBox();
+  };
+
+  SceneObjects.prototype.makePlayer = function() {
+    var playerWidth = SceneObjects.PLAYER_WIDTH;
+    SceneObjects.player = new THREE.Mesh(new THREE.CubeGeometry(playerWidth, playerWidth, playerWidth), Materials.BASIC);
+    SceneObjects.player.add(SceneObjects.camera);
+    this.scene.add(SceneObjects.player);
+  };
+
+  SceneObjects.prototype.makeSkyBox = function() {
+    var skyGeometry = new THREE.CubeGeometry(50000, 50000, 50000);
+    var skyBox = new THREE.Mesh (skyGeometry, Materials.SKY);
+    this.scene.add(skyBox);
+  };
+
+  // Static methods follow
 
   SceneObjects.toggleFalling = function(threeObject) {
     var bullet = _.find(SceneObjects.allBullets, function(element) {
