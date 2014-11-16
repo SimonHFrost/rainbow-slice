719ccaac1933567fa37e60ca149c171f28d95e0f
simonhfrost <simonhfrost@gmail.com>
2014-11-16 18:41:28 +0100

    Use config.js for environment specific files and untrack it

---
+++ configExample.js
@@ -0,0 +1,10 @@
+// This is an example config file that can be used in development
+// Just rename this file to 'config.js' and you're ready to go.
+
+var config = {};
+
+config.DEBUG_MODE = true;
+config.PORT = 8080;
+config.PUBLIC_FOLDER_LOCATION = './public';
+
+module.exports = config;
